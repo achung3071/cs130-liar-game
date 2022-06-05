4aebba0015a81d7d0feb423dcac90b6fe039e198
Andrew Chung <andrewchung2022@u.northwestern.edu>
2022-06-05 03:58:02 -0500

    add spy image to logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import { Layout, Typography } from "antd";
+import { Layout, Typography, Image } from "antd";
 import "./App.css";
 import "antd/dist/antd.min.css";
 import Home from "./components/Home";
 import Categories from "./components/Categories";
 import Reveal from "./components/Reveal";
 import Round from "./components/Round";
+import spyImg from "./assets/images/spy.png";
 
 const { Header, Content } = Layout;
 const { Title } = Typography;
@@ -14,9 +15,24 @@ function App() {
     return (
         <Layout style={{ height: "100vh" }}>
             <Header style={{ paddingTop: 10 }}>
-                <Title level={2} style={{ color: "white" }}>
+                <Image
+                    style={{ marginTop: -20, paddingRight: 5 }}
+                    height={40}
+                    src={spyImg}
+                    preview={false}
+                />
+                <Title
+                    level={2}
+                    style={{ display: "inline-block", color: "white" }}
+                >
                     Liar Game
                 </Title>
+                <Image
+                    style={{ marginTop: -20, paddingLeft: 5 }}
+                    height={40}
+                    src={spyImg}
+                    preview={false}
+                />
             </Header>
             <Content style={{ paddingTop: 50, height: "100%" }}>
                 <Routes>
